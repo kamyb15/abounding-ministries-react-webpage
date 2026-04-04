2285830ffb19e5ec1e97f344570c9084c0bc9db3
Briathna Bubick <149510120+kamyb15@users.noreply.github.com>
2026-04-03 21:29:37 -0400

    update build

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,5 @@
-import { Hero } from './components/Hero';
-import { About } from './components/About';
-import { GirlsNight } from './components/GirlsNight';
-import { Blog } from './components/Blog';
-import { Contact } from './components/Contact';
-import { Navigation } from './components/Navigation';
 
 
 export default function App() {
-    return (
-        <div className='min-h-screen bg-[#FBF8F3]'> {/* minimum height: 100% of viewport height; background-color: #FBF8F3*/}
-            <Navigation />
-            <Hero />
-            <About />
-            <GirlsNight />
-            <Blog />
-            <Contact />
-        </div>
-    );
+  return <h1>HELLO WORLD</h1>;
 }
